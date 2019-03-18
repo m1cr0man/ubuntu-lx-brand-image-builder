f7c65bdfcde6986c1d9188a5f252dc27b9df35ae
Lucas Savva <lucas@m1cr0man.com>
2019-03-18 14:00:28 +0000

    Bump minimum version

--- create.sh
+++ create.sh
@@ -2,6 +2,6 @@
 
 FILE=*.tar.gz
 
-./create-lx-image -t $(pwd)/${FILE} -k 4.4.0 -m 20181011T004530Z -i lx-ubuntu -d "Ubuntu 18.10 Cosmic Cuttlefish M1cr0man 64-bit lx-brand image." -u https://docs.joyent.com/images/container-native-linux
+./create-lx-image -t $(pwd)/${FILE} -k 4.4.0 -m 20190314T022406Z -i lx-ubuntu -d "Ubuntu 18.10 Cosmic Cuttlefish M1cr0man 64-bit lx-brand image." -u https://docs.joyent.com/images/container-native-linux
 
 rm -f ${FILE}
